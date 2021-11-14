78346b0754c852db7e0ac1811015aed72fa357e7
Alinav28 <80707417+Alinav28@users.noreply.github.com>
2021-11-14 13:51:39 +0200

    hw-js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './sass/main.scss';
+import menuCards from './menuCards.hbs';
+import menu from './menu.json';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const themeSwitcherCheckboxRef = document.querySelector('.theme-switch__toggle');
+const menuCardsRef = document.querySelector('.menu');
+menuCardsRef.insertAdjacentHTML('beforeend', menuCards(menu));
+
+if (localStorage.getItem('theme') === 'dark') {
+  bodyRef.classList.toggle(`${Theme.DARK}`);
+  themeSwitcherCheckboxRef.checked = true;
+}
+
+themeSwitcherCheckboxRef.addEventListener('change', () => {
+  bodyRef.classList.toggle(`${Theme.DARK}`);
+  themeSwitcherCheckboxRef.checked
+    ? localStorage.setItem('theme', 'dark')
+    : localStorage.setItem('theme', 'light');
+});
